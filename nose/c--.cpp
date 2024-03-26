8eeba91821200b9a27db50bd4a8664708fcba0c0
dojacalf <99399626+dojacalf@users.noreply.github.com>
2024-03-26 23:25:19 +0000

    hladfhd

--- nose/c--.cpp
+++ nose/c--.cpp
@@ -5,8 +5,19 @@
 #include <vector>
 
 using namespace std;
+//dividir dos numeros sin usar el sinbolo de divicion
+int divide(int a,int b){
+    int rpta;
+    while(a>b){
+        a = a-b;
+           rpta++ ;
 
+    }
+    return rpta;
+}
 int main() {
-    cout << "hola";
+    int a,b ;
+    cin >> a>>b;
+    cout << "la divicion entrea a y b es :"<< divide(a,b);
   return 0;
 }
